9ee8f2832cf2301f0a469eb7693cd93c35f82fc2
@nataliia.vyrsta <natali.vyrsta@gmail.com>
2022-10-06 15:58:56 +0300

    add day 8 - Form Wave Animation

---
+++ day_8/script.js
@@ -0,0 +1,13 @@
+'use strict';
+const labels = document.querySelectorAll('.form-control label');
+labels.forEach((label) => {
+  label.innerHTML = label.innerText
+    .split('')
+    .map(
+      (letter, ind) =>
+        `
+  <span style="transition-delay:${ind * 50}ms">${letter}</span>
+  `
+    )
+    .join('');
+});
